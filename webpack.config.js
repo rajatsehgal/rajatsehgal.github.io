77fd7e0712b7e19a863fab45ee21b584454019a6
Rajat Sehgal <rajatsehgal1988@gmail.com>
2016-04-07 19:47:50 -0400

    Upgrade to react 15.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
+var webpack = require("webpack");
+
 module.exports = {
   entry: './js/main.js',
   output: {
     path: __dirname + '/built',
     filename: 'app.js'
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
   module: {
     loaders: [
       {
