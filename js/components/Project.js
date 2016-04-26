7fff85d4f97b1acb861c373dfdfd8049480939ff
Rajat Sehgal <rajatsehgal1988@gmail.com>
2016-04-26 00:03:03 -0400

    Add github links to projects

--- js/components/Project.js
+++ js/components/Project.js
@@ -4,19 +4,28 @@ import colors from '../utils/colors';
 import Carousel from './Carousel';
 import SkillBar from './SkillBar';
 
-const Project = ({ title, subTitle, description, images, skills, headerHeight }) => (
-  <div style={styles.root}>
-    <div id={title.replace(/\./g, '')}
-         style={{ height: headerHeight + 10, marginTop: -headerHeight - 10 }}></div>
-    <div style={styles.title}>{title}</div>
-    <div style={styles.subTitle}>{subTitle}</div>
-    <SkillBar skills={skills}/>
-    <div>{description.text.join(' ')}</div>
-    <div style={styles.carouselContainer}>
-      <Carousel items={images.list} initialIndex={images.initialIndex}/>
+const Project = ({ title, subTitle, description, images, skills, headerHeight, githubName }) => {
+  const link = githubName ? <a
+    target="_blank"
+    href={`https://github.com/rajatsehgal/${githubName}`}
+    style={styles.link}
+  >
+    <i className={'fa fa-github'}/>
+  </a> : null;
+  return (
+    <div style={styles.root}>
+      <div id={title.replace(/\./g, '')}
+           style={{ height: headerHeight + 10, marginTop: -headerHeight - 10 }}></div>
+      <div><span style={styles.title}>{title}</span>{link}</div>
+      <div style={styles.subTitle}>{subTitle}</div>
+      <SkillBar skills={skills}/>
+      <div>{description.text.join(' ')}</div>
+      <div style={styles.carouselContainer}>
+        <Carousel items={images.list} initialIndex={images.initialIndex}/>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 const styles = {
   root: {
@@ -33,6 +42,13 @@ const styles = {
   carouselContainer: {
     textAlign: 'center',
     marginTop: 5
+  },
+  link: {
+    fontSize: 28,
+    borderBottom: 'none',
+    textDecoration: 'none',
+    color: colors.link,
+    marginLeft: 10
   }
 };
 
